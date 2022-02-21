c0056f474f90690b4d10e42adc2ac50dfce5f6ff
Joseph Shumway <joseph.b.shumway@gmail.com>
2022-02-21 16:42:11 -0600

    Added postgresql.jar and base java class from lab

---
+++ jdbcpostgreSQL.java
@@ -0,0 +1,79 @@
+import java.sql.*;
+
+/*
+CSCE 315
+9-27-2021 Lab
+ */
+public class jdbcpostgreSQL {
+
+  //Commands to run this script
+  //This will compile all java files in this directory
+  //javac *.java
+  //This command tells the file where to find the postgres jar which it needs to execute postgres commands, then executes the code
+  //Windows: java -cp ".;postgresql-42.2.8.jar" jdbcpostgreSQL
+  //Mac/Linux: java -cp ".:postgresql-42.2.8.jar" jdbcpostgreSQL
+
+  //MAKE SURE YOU ARE ON VPN or TAMU WIFI TO ACCESS DATABASE
+  public static void main(String args[]) {
+
+    //Building the connection with your credentials
+    //TODO: update teamNumber, sectionNumber, and userPassword here
+     Connection conn = null;
+     String teamNumber = "21";
+     String sectionNumber = "904";
+     String dbName = "csce315" + sectionNumber + "_" + teamNumber + "db";
+     String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+     String userName = "csce315" + sectionNumber + "_" + teamNumber + "user";
+     String userPassword = "315gang";
+
+    //Connecting to the database
+    try {
+        conn = DriverManager.getConnection(dbConnectionString,userName, userPassword);
+     } catch (Exception e) {
+        e.printStackTrace();
+        System.err.println(e.getClass().getName()+": "+e.getMessage());
+        System.exit(0);
+     }
+
+     System.out.println("Opened database successfully");
+
+     try{
+       //create a statement object
+       Statement stmt = conn.createStatement();
+
+       //Running a query
+       //TODO: update the sql command here
+       //String sqlStatement = "INSERT INTO teammembers VALUES ('John Smith', 904, 'Inception', '04/01/2022');";
+
+       String sqlStatement = "SELECT * FROM teammembers LIMIT 10;";
+
+       //send statement to DBMS
+       //This executeQuery command is useful for data retrieval
+       ResultSet result = stmt.executeQuery(sqlStatement);
+       //OR
+       //This executeUpdate command is useful for updating data
+       //int result = stmt.executeUpdate(sqlStatement);
+
+       //OUTPUT
+       //You will need to output the results differently depeninding on which function you use
+       System.out.println("--------------------Query Results--------------------");
+       while (result.next()) {
+         System.out.println(result.getString("student_name"));
+       }
+       //OR
+       // System.out.println(result);
+   } catch (Exception e){
+       e.printStackTrace();
+       System.err.println(e.getClass().getName()+": "+e.getMessage());
+       System.exit(0);
+   }
+
+    //closing the connection
+    try {
+      conn.close();
+      System.out.println("Connection Closed.");
+    } catch(Exception e) {
+      System.out.println("Connection NOT Closed.");
+    }//end try catch
+  }//end main
+}//end Class
