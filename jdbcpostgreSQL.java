6565d265362fc92317cd1eb8fa83ff9cee83c6a3
Logan C <loganyc@tamu.edu>
2022-02-21 18:02:33 -0600

    input elements set up

--- jdbcpostgreSQL.java
+++ jdbcpostgreSQL.java
@@ -1,18 +1,15 @@
 import java.sql.*;
 import java.io.*;  
 import java.util.Scanner;  
-/*
-CSCE 315
-9-27-2021 Lab
- */
+
 public class jdbcpostgreSQL {
 
   //Commands to run this script
   //This will compile all java files in this directory
   //javac *.java
   //This command tells the file where to find the postgres jar which it needs to execute postgres commands, then executes the code
-  //Windows: java -cp ".;postgresql-42.2.8.jar" jdbcpostgreSQL
-  //Mac/Linux: java -cp ".:postgresql-42.2.8.jar" jdbcpostgreSQL
+  //Windows: java -cp ".;postgresql-42.2.8.jar" jdbcpostgreSQL.java
+  //Mac/Linux: java -cp ".:postgresql-42.2.8.jar" jdbcpostgreSQL.java
 
   //MAKE SURE YOU ARE ON VPN or TAMU WIFI TO ACCESS DATABASE
 
@@ -26,18 +23,37 @@ public class jdbcpostgreSQL {
   public static String userPassword = "315gang";
   //___________________________________________________
 
+
+
+
+
+
+
+  
   //function to input elements into the Menu Table.  
   public static void inputElementsIntoMenuTable(String fileName){
     Scanner sc;
     
     try{
       sc = new Scanner(new File(fileName));
-
+      String tableName = sc.nextLine();
+      String[] parseArr = tableName.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
       // populates in the following order Item, name, Description, price 
-      while(sc.hasNext()){
+      String tableFormatting = sc.nextLine();
+      parseArr = tableFormatting.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+      String sqlStatement = "SELECT * FROM teammembers LIMIT 10;";
+      
+      while(sc.hasNextLine()){
         
-        String[] parseArr = sc.nextLine().split(",");
-        System.out.println(parseArr.toString());
+        //creates array of elements in a line
+        parseArr = sc.nextLine().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+        
+        
+        for (String string : parseArr) {
+          if (string.length() != 0) {
+            System.out.println(string);
+          }
+        }
       }
 
 
@@ -47,49 +63,51 @@ public class jdbcpostgreSQL {
     catch (FileNotFoundException e) {e.printStackTrace();}
   }
 
-  // leetCode to set up the main menu items
+
+
+
 
   public static void setupDatabase() {
-    //Building the connection with your credentials
-    //TODO: update teamNumber, sectionNumber, and userPassword here
 
-   //Connecting to the database
    try {
-       conn = DriverManager.getConnection(dbConnectionString,userName, userPassword);
+      conn = DriverManager.getConnection(dbConnectionString,userName, userPassword);
     } catch (Exception e) {
-       e.printStackTrace();
-       System.err.println(e.getClass().getName()+": "+e.getMessage());
-       System.exit(0);
+      e.printStackTrace();
+      System.err.println(e.getClass().getName()+": "+e.getMessage());
+      System.exit(0);
     }
 
     System.out.println("Opened database successfully");
   }
 
+
+
+
+
   public static void runSQLCommands() {
     try{
-      //create a statement object
+      // create a statement object
       Statement stmt = conn.createStatement(); 
 
-      //Running a query
-      //TODO: update the sql command here
-      //String sqlStatement = "INSERT INTO teammembers VALUES ('John Smith', 904, 'Inception', '04/01/2022');";
+      // Running a query
+      // String sqlStatement = "INSERT INTO teammembers VALUES ('John Smith', 904, 'Inception', '04/01/2022');";
 
       String sqlStatement = "SELECT * FROM teammembers LIMIT 10;";
 
-      //send statement to DBMS
-      //This executeQuery command is useful for data retrieval
+      // send statement to DBMS
+      // This executeQuery command is useful for data retrieval
       ResultSet result = stmt.executeQuery(sqlStatement);
-      //OR
-      //This executeUpdate command is useful for updating data
-      //int result = stmt.executeUpdate(sqlStatement);
+      // OR
+      // This executeUpdate command is useful for updating data
+      // int result = stmt.executeUpdate(sqlStatement);
 
-      //OUTPUT
-      //You will need to output the results differently depeninding on which function you use
+      // OUTPUT
+      // You will need to output the results differently depeninding on which function you use
       System.out.println("--------------------Query Results--------------------");
       while (result.next()) {
         System.out.println(result.getString("student_name"));
       }
-      //OR
+      // OR
       // System.out.println(result);
     } catch (Exception e){
         e.printStackTrace();
@@ -97,31 +115,32 @@ public class jdbcpostgreSQL {
         System.exit(0);
     }
   }
-public static void main(String args[]) {
-
-  setupDatabase();
-
-  
 
 
-  // _________running commands________
-  runSQLCommands();
-
-
-
-
-
-  // _________________Close Connection__________________
-
-  //closing the connection
-  try {
-    conn.close();
-    System.out.println("Connection Closed.");
-  } catch(Exception e) {
-    System.out.println("Connection NOT Closed.");
+  public static void closeConnection() {
+    try {
+      conn.close();
+      System.out.println("Connection Closed.");
+    } catch(Exception e) {
+      System.out.println("Connection NOT Closed.");
+    }
   }
 
-  // ___________________________________________________
 
-}//end main
-}//end Class
+public static void main(String args[]) {
+
+  inputElementsIntoMenuTable("./CSCE315-1/MenuKey.csv");
+
+  // _________setup the database_______
+  //setupDatabase();
+
+
+  // _________running commands_________
+  //runSQLCommands();
+
+
+  // __________Close Connection________
+  //closeConnection();
+
+  }
+}
