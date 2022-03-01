ca32141c59df37fed5a388a224a0e75b2fba8b2f
Logan <loganyc@tamu.edu>
2022-03-01 15:33:01 -0600

    modified server layout, added button action base

--- Server.java
+++ Server.java
@@ -1,38 +1,109 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
 
-public class Server {
+public class Server implements ActionListener{
 
     private JPanel main;
     private JPanel leftPane;
-    private JPanel rightPane;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
-    private JButton button10;
-    private JButton button11;
-    private JButton button12;
-    private JButton button13;
-    private JButton button14;
-    private JButton button15;
-    private JButton button16;
-    private JButton button17;
-    private JButton button18;
-    private JButton button19;
-    private JButton button20;
-    private JButton button21;
-    private JButton button22;
-    private JButton button23;
-    private JButton button24;
-    private JButton button25;
-    private JTextArea ticketTextArea;
-    private JTextArea orderTextArea;
-    private JEditorPane editorPane1;
+    private JButton fiveFinger;
+    private JButton fourFinger;
+    private JButton threeFingerMealButton;
+    private JButton clubSandwichMealButton;
+    private JButton kidsMealButton;
+    private JButton sandwichMealComboButton;
+    private JButton chickenFingerButton;
+    private JButton fountainDrinkButton;
+    private JButton minus;
+    private JButton plus;
+    private JButton bottleDrinkButton;
+    private JButton gallonOfTeaButton;
+    private JButton layneSSauceButton;
+    private JButton texasToastButton;
+    private JButton potatoSaladButton;
+    private JButton crinkleCutFriesButton;
+    private JButton familyPackButton;
+    private JButton grillCheeseSandwichOnlyButton;
+    private JButton sandwichOnlyButton;
+    private JButton clubSandwichOnlyButton;
+    private JButton grillCheeseMealComboButton;
+    private JTable ticket;
+    private JTextPane textPane1;
+    private JButton finalizeOrderButton;
+    private JScrollPane ticketScroll;
+    private DefaultTableModel serverTableModel;
+    final private String serverTicket[] = {"Item", "Amount"};
+    private String insertion[] = {"Blank", "0"};
+    private ArrayList<JButton> buttonList = new ArrayList<JButton>();
+
+    public Server(){
+        serverTableModel = new DefaultTableModel(serverTicket, 0);
+        serverTableModel.setColumnIdentifiers(serverTicket);
+        ticketScroll.setColumnHeaderView(ticket.getTableHeader());
+        ticket.setModel(serverTableModel);
+        buttonSetup();
+        for(JButton b : buttonList){
+            b.addActionListener(this);
+        }
+
+
+    }
+    public void buttonSetup(){
+        buttonList.add(fiveFinger);
+        fiveFinger.setName("501");
+        buttonList.add(fourFinger);
+        fourFinger.setName("502");
+        buttonList.add(threeFingerMealButton);
+        threeFingerMealButton.setName("503");
+        buttonList.add(kidsMealButton);
+        kidsMealButton.setName("504");
+        buttonList.add(gallonOfTeaButton);
+        gallonOfTeaButton.setName("505");
+        buttonList.add(familyPackButton);
+        familyPackButton.setName("506");
+        buttonList.add(clubSandwichMealButton);
+        clubSandwichMealButton.setName("507");
+        buttonList.add(clubSandwichOnlyButton);
+        clubSandwichOnlyButton.setName("508");
+        buttonList.add(sandwichMealComboButton);
+        sandwichMealComboButton.setName("509");
+        buttonList.add(sandwichOnlyButton);
+        sandwichOnlyButton.setName("510");
+        buttonList.add(grillCheeseMealComboButton);
+        grillCheeseMealComboButton.setName("511");
+        buttonList.add(grillCheeseSandwichOnlyButton);
+        grillCheeseSandwichOnlyButton.setName("512");
+        buttonList.add(layneSSauceButton);
+        layneSSauceButton.setName("513");
+        buttonList.add(chickenFingerButton);
+        chickenFingerButton.setName("514");
+        buttonList.add(texasToastButton);
+        texasToastButton.setName("515");
+        buttonList.add(potatoSaladButton);
+        potatoSaladButton.setName("516");
+        buttonList.add(crinkleCutFriesButton);
+        crinkleCutFriesButton.setName("517");
+        buttonList.add(fountainDrinkButton);
+        fountainDrinkButton.setName("518");
+        buttonList.add(bottleDrinkButton);
+        bottleDrinkButton.setName("519");
+        buttonList.add(minus);
+        minus.setName("-");
+        buttonList.add(plus);
+        plus.setName("+");
+        buttonList.add(finalizeOrderButton);
+        finalizeOrderButton.setName("finalize");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(insertion[0] == "Blank"){
+            System.out.println(((JButton)e.getSource()).getName());
+        }
+    }
+
+
 
     public JPanel getRootPanel() {
         return main;
