6f3caafd6335a986ce838d9b9543c810bac26f02
barna-nicholas <barna_nicholas@tamu.edu>
2022-03-07 20:18:28 -0600

    finish inv update need debug

--- jdbcpostgreSQL.java
+++ jdbcpostgreSQL.java
@@ -224,6 +224,20 @@ public class jdbcpostgreSQL {
 
     return null;
   }
+  public static void executeSingleInvUpdate(String item, double oldTotal, double base, double amt){
+
+    try {
+      Statement stmt = jdbcpostgreSQL.conn.createStatement();
+      double newVal = oldTotal - (base * amt);
+      int result = stmt.executeUpdate("UPDATE inventory SET quantity=" + Double.toString(newVal) + " WHERE description=\'" + item + "\';");
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    }
+
+
   // purpse of the func is to update inven database given global vars date.
   public static void updateInventoryGivenDate(){
     try{
@@ -231,6 +245,7 @@ public class jdbcpostgreSQL {
       ArrayList<ArrayList<String>> MenuItemConvert =  getItemConversion();
       ArrayList<ArrayList<String>> menuKey =  getMenuKey();
       ArrayList<ArrayList<String>> itemsToUpdate = new ArrayList<ArrayList<String>>();
+      ArrayList<ArrayList<String>> inventory = getDBInventory();
       if(weeksales == null){
         return;
       }
@@ -243,7 +258,7 @@ public class jdbcpostgreSQL {
       ArrayList<ArrayList<String>> temp = new ArrayList<ArrayList<String>>();
       for(int i = 0; i < itemsToUpdate.size(); i++){
         ArrayList<String> tempLine = new ArrayList<String>();
-        //templine = food number, base ingredients, base additionals, quantity
+        //templine = food number, base ingredients, base additionals, quantity, oldAmt
 
         tempLine.add(itemsToUpdate.get(i).get(0).substring(itemsToUpdate.get(i).get(0).indexOf('_')+1));
         
@@ -262,12 +277,24 @@ public class jdbcpostgreSQL {
         }
 
         tempLine.add(itemsToUpdate.get(i).get(1)); // adds quantity
+        
+        for(int oldAmt = 0; oldAmt < inventory.size(); oldAmt++){
+          if(tempLine.get(1).split(";")[0].split("=")[0].trim().equals(inventory.get(i).get(0))){
+            tempLine.add(inventory.get(i).get(2)); // adds old total 
+          }
+
+      }
+        
         temp.add(tempLine);
       }
       itemsToUpdate = temp;
       temp = new ArrayList<ArrayList<String>>();
+      
       for(int i = 0; i < itemsToUpdate.size(); i++){
-        
+        double base = Double.parseDouble(itemsToUpdate.get(i).get(4));
+
+        executeSingleInvUpdate(itemsToUpdate.get(i).get(1).split(";")[0].split("=")[0], base, Double.parseDouble(itemsToUpdate.get(i).get(1).split(";")[0].split("=")[1]), Double.parseDouble(itemsToUpdate.get(i).get(3)));
+
       }
 
       
